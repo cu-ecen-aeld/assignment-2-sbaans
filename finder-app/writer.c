8f126f0e38bc49c066b513d153552e8de16e9a3e
Sebastien BANON <sebastien.banon@se.com>
2024-06-07 15:57:17 +0200

    add the files for assignment 2
    Signed-off-by: Sebastien BANON <sebastien.banon@se.com>

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -8,7 +8,43 @@
 * parameter 2: The string we want to write
 */
 
-void main()
-{
+#include <stdio.h>
+#include <syslog.h>
 
+
+
+int main(int argc, char *argv[])
+{
+	FILE *filetobewritten;
+
+	/* Activate syslog */
+	openlog(NULL,0,LOG_USER);
+
+	/* Check if there are 2 arguments ( argc should be 3 because the name of the program is one argument also)
+	 *
+	 */
+	if (argc != 3)
+	{
+		printf("Error, there should be 2 arguments");
+		syslog(LOG_ERR,"Invalid Number of arguments: %d , it should be 2", (argc - 1));
+		return 1;
+	}
+	syslog(LOG_DEBUG,"Good Number of parameters");
+	/* parameter 1 is the name of the file with path */
+	/* parameter 2 is the string to write */
+
+	filetobewritten = fopen(argv[1], "wa");
+	if (filetobewritten != NULL)
+	{
+		fputs(argv[2], filetobewritten);
+		fclose(filetobewritten);
+		printf("Writing %s to %s",argv[2],argv[1]);
+		syslog(LOG_DEBUG,"Writing %s to %s",argv[2],argv[1]);
+	}
+	else
+	{
+		printf("Error, Unable to open File");
+		syslog(LOG_ERR,"Unable to open File %s", argv[1]);
+	}
+	return 0;
 }
